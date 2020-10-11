0ba73d49e7f3f0a8979ae95d0008b584c2a2e103
Abdelhakim RASFI <abdelhakim.rasfi@alumni.univ-avignon.fr>
2020-10-12 01:51:43 +0200

    Parser Class created

---
+++ src/fr/univavignon/ceri/webcrawl/Parser.java
@@ -0,0 +1,17 @@
+package fr.univavignon.ceri.webcrawl;
+
+/**
+ * This class is used to parse a web page.
+ * 
+ * @author Abdelhakim RASFI
+ * 
+ */
+
+public class Parser {
+
+	//TODO
+	public Parser() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
