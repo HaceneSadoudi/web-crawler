b59b1c3e78a508a36abcb4bf44e6fb538cbeddff
Abdelhakim RASFI <abdelhakim.rasfi@alumni.univ-avignon.fr>
2020-10-30 23:19:37 +0100

    Deleted unused "Imports"

--- src/fr/univavignon/ceri/webcrawl/Parser.java
+++ src/fr/univavignon/ceri/webcrawl/Parser.java
@@ -2,7 +2,6 @@ package fr.univavignon.ceri.webcrawl;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.http.HttpClient;
