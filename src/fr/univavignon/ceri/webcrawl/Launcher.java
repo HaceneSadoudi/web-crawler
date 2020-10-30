03050e36765ea9d27f72cba3cd32a1ad5f79c438
Imane HACEN <imane.hacen@alumni.univ-avignon.fr>
2020-10-30 14:46:07 +0100

    Main pour graph

--- src/fr/univavignon/ceri/webcrawl/Launcher.java
+++ src/fr/univavignon/ceri/webcrawl/Launcher.java
@@ -17,7 +17,13 @@ public class Launcher
 	 * @param args
 	 * 		Not used here.
 	 */
+	
 	public static void main(String[] args)
 	{	//TODO to be completed
+		
+		//main test graphe.
+		ArrayList<String> tabUrl=new ArrayList<String>();
+		Graphh graph=new Graphh("https://www.nytimes.com");
+		graph.graphSite(tabUrl);
 	}
 }
