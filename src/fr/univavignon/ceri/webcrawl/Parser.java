ed537a0cdb968bbabcba94036947a4d8d0466915
Youssef Abidar <youssef.abidar@alumni.univ-avignon.fr>
2020-12-25 13:05:02 +0000

    Update Parser.java

--- src/fr/univavignon/ceri/webcrawl/Parser.java
+++ src/fr/univavignon/ceri/webcrawl/Parser.java
@@ -1,5 +1,8 @@
+
 package fr.univavignon.ceri.webcrawl;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -9,6 +12,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,13 +30,22 @@ import java.util.regex.Pattern;
 public class Parser {
 
 	// TODO
-	// Declaration des attributs de la classe Parser
+	// Variables declaration
 	private String url;
 	private String body;
+	public String bodyRobotsTxt;
+	
+	private ArrayList<String> sitemapUrls;
+	
+	public static boolean RESPECT_ROBOT_TXT = true;
+	public static boolean NORESPECT_ROBOT_TXT = false;
+	public static boolean RESPECT_SITEMAP = true;
+	public static boolean NORESPECT_SITEMAP = false;
 
-	public Parser(String _url) {
+	public Parser(String _url, boolean robottxt, boolean sitemap) {
 		this.url = _url;
 		String content = null;
+		String contentRobotsTxt = null;
 		try {
 			// Creation de la l'object client a partir de la classe HttpClient 
 			// qui va nous servir a acceder au contenu du siteweb
@@ -47,22 +60,84 @@ public class Parser {
 			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 			// Stockage du contenu de la page web dans la variable content
 			content = response.body();
+			
+			String urlRobotsTxt = url.split("/")[0] + "//" + url.split("/")[1] + url.split("/")[2];
+			urlRobotsTxt += "/robots.txt";
+			System.out.println(urlRobotsTxt);
+			HttpRequest request1 = HttpRequest.newBuilder()
+					.uri(URI.create(urlRobotsTxt))
+					.GET().build();
+			// Recuperation de la reponse pour la stocker dans une variable locale
+			HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
+			// Stockage du contenu de la page web dans la variable content
+			contentRobotsTxt = response1.body();
 		} catch (Exception ex) {
-			ex.printStackTrace();
+			//ex.printStackTrace();
 		}
+		this.body = content;
+		this.bodyRobotsTxt = contentRobotsTxt;
+		
 		// remplacement des retours a la ligne par des espaces pour
 		// utiliser les expressions regulieres qui vont reconnaitre les liens
 		// content = content.replace("\n", " ").replace("\n\r", " ");
-		this.body = content;
 	}
 
-	public String getTitle()
+	static public String getTitle(String url)
 	{
-		int title;
-		title = "";
+		String content = null;
+		String title = null;
+		try {
+			HttpClient client = HttpClient.newBuilder()
+					.connectTimeout(Duration.ofSeconds(20))
+					.followRedirects(Redirect.ALWAYS)
+					.build();
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create(url))
+					.GET().build();
+			HttpResponse<String> response;
+				response = client.send(request, HttpResponse.BodyHandlers.ofString());
+				content = response.body();
+		String contentToLower = content.toLowerCase();
+		int begin = contentToLower.indexOf("<title") + 7;
+		int end = contentToLower.indexOf("</title>");
+		//System.out.println(begin + " " + end);
+		title = content.substring(begin, end).trim();
+		title = title.substring(title.indexOf('>') + 1, title.length());
+		System.out.println(title);
+		} catch (Exception e) {
+			//e.printStackTrace();
+		}
 		return title;
 	}
 
+	public ArrayList<String> linksOnRobotsTxt(){
+		ArrayList<String> urls = new ArrayList<String>();
+		String[] lines =  this.bodyRobotsTxt.split("\n");
+		int isInBlock = 0;
+		for (String line : lines)
+		{
+			if (line.toLowerCase().indexOf("user-agent:") != -1 && isInBlock == 1)
+				break;
+			if (line.toLowerCase().indexOf("user-agent: *") == -1 && isInBlock == 0)
+			{
+				continue;
+			}
+			if (isInBlock == 0)
+			{
+				isInBlock = 1;
+				continue;
+			}
+			if (line.toLowerCase().indexOf("disallow") != -1)
+			{
+				line = line.replace("Disallow: ", "");
+				line = url.split("/")[0] + "//" + url.split("/")[1] + url.split("/")[2] + line;
+				urls.add(line);
+				System.out.println(line);
+			}
+		}
+		return urls;
+
+	}
 	public ArrayList<String> linksOnPage() {
 		// Creation de la liste result c'est la ou on va stocker les liens
 		ArrayList<String> result = new ArrayList<String>();
