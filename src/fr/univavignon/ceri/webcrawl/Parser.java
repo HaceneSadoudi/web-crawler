f0424a0ab288fc158559a19648856e16414d5f36
Abdelhakim RASFI <hakim.rasfi@gmail.com>
2021-01-14 20:02:38 +0100

    Removed useless code

--- src/fr/univavignon/ceri/webcrawl/Parser.java
+++ src/fr/univavignon/ceri/webcrawl/Parser.java
@@ -29,8 +29,7 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-	// TODO
-	// Variables declaration
+
 	private String url;
 	private String body;
 	public String bodyRobotsTxt;
@@ -47,8 +46,6 @@ public class Parser {
 		String content = null;
 		String contentRobotsTxt = null;
 		try {
-			// Creation de la l'object client a partir de la classe HttpClient 
-			// qui va nous servir a acceder au contenu du siteweb
 			HttpClient client = HttpClient.newBuilder()
 					.connectTimeout(Duration.ofSeconds(20))
 					.followRedirects(Redirect.ALWAYS)
@@ -56,9 +53,7 @@ public class Parser {
 			HttpRequest request = HttpRequest.newBuilder()
 					.uri(URI.create(this.url))
 					.GET().build();
-			// Recuperation de la reponse pour la stocker dans une variable locale
 			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-			// Stockage du contenu de la page web dans la variable content
 			content = response.body();
 			
 			String urlRobotsTxt = url.split("/")[0] + "//" + url.split("/")[1] + url.split("/")[2];
@@ -67,19 +62,13 @@ public class Parser {
 			HttpRequest request1 = HttpRequest.newBuilder()
 					.uri(URI.create(urlRobotsTxt))
 					.GET().build();
-			// Recuperation de la reponse pour la stocker dans une variable locale
 			HttpResponse<String> response1 = client.send(request1, HttpResponse.BodyHandlers.ofString());
-			// Stockage du contenu de la page web dans la variable content
 			contentRobotsTxt = response1.body();
 		} catch (Exception ex) {
-			//ex.printStackTrace();
 		}
 		this.body = content;
 		this.bodyRobotsTxt = contentRobotsTxt;
 		
-		// remplacement des retours a la ligne par des espaces pour
-		// utiliser les expressions regulieres qui vont reconnaitre les liens
-		// content = content.replace("\n", " ").replace("\n\r", " ");
 	}
 
 	static public String getTitle(String url)
@@ -139,44 +128,29 @@ public class Parser {
 
 	}
 	public ArrayList<String> linksOnPage() {
-		// Creation de la liste result c'est la ou on va stocker les liens
 		ArrayList<String> result = new ArrayList<String>();
-		// Expression reguliere qui recuperes toutes les balises de type <a>...<a>
 		String globalRegex = "<a (.*?)</a>";
 		Pattern globalPattern = Pattern.compile(globalRegex, Pattern.CASE_INSENSITIVE);
 		Matcher globalMatcher = globalPattern.matcher(this.body);
 		while (globalMatcher.find()) {
-			// Expresion reguliere pour recuperer le contenu du href
 			String regex = "href=\"(.*?)\"";
-			// Ici on ignore les majiscules et les minuscules pour qu'on puisse 
-			// traiter tous type des balise et href soit miniscules ou bien majiscules ou
-			// les deux ensemble
 			Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
 			Matcher m = p.matcher(globalMatcher.group());
 			while (m.find()) {
-				// ici on ignore les liens qui commencent par # par ce
-				// qu'ils menent pas vers des autres pages
 				if (m.group(1).length() > 0 && m.group(1).charAt(0) != '#') 
 				{
 					String currentHref = m.group(1);
-					// On reformule les liens qui commencent avec des // 
-					// on doit ajouter soit http ou bien https
 					if (currentHref.length() >= 2 && currentHref.substring(0, 2).equals("//")) 
 					{
 						currentHref = this.url.split("//")[0] + m.group(1);
 						result.add(currentHref);
 					} else 
 					{
-						//ici on verifie si le contenu du href s'agit bien
-						// bien d'un lien
 						try 
 						{
 							new URL(currentHref);
 							result.add(m.group(1));
 						} 
-						// sinon on essaye de le reformuler par ce que
-						// on sait pas s'il est un lien relatif a l'url de base
-						// ou pas
 						catch (MalformedURLException malformedURLException) 
 						{
 							URL url = null;
@@ -186,7 +160,7 @@ public class Parser {
 							} 
 							catch (MalformedURLException e1) 
 							{
-								// TODO
+
 							}
 						}
 					}
